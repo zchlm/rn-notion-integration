0be4945ac7ae318635f83af89644c1228915580b
Z <8256446+zchlm@users.noreply.github.com>
2022-01-31 22:02:20 +1100

    Update queue to 2 minutes

--- src/queue.ts
+++ src/queue.ts
@@ -2,15 +2,12 @@ import * as cron from "cron"
 import { main } from "./index"
 
 const job = new cron.CronJob(
-  "*/1 * * * *",
+  "*/2 * * * *",
   async function () {
-    console.log("You will see this message every 1 minutes")
+    console.log("You will see this message every 2 minutes")
     await main()
   },
   null,
   true,
   "UTC"
 )
-
-// Use this if the 4th param is default value(false)
-// job.start()
